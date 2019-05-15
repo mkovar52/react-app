38390fd0b14522a96eb282193d3001c6cfb2729f
Mykel Kovar <mykelkovar@mykelkovar.attlocal.net>
2019-05-15 05:57:00 -0500

    basic Header and Contact component setup

---
+++ src/components/Contact.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+class Contact extends Component {
+  render() {
+    const { name, email, phone } = this.props;
+    
+    return (
+      <div>
+        <h3>{name}</h3>
+        <li>Phone: {phone} </li>
+        <li>Email: {email}</li>
+      </div>
+    )
+  }
+}
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
+};
+
+export default Contact;
