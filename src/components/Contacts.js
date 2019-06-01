c3e44e7eea36e9e051a54c8c1a4933947cc6ee0f
Mykel Kovar <mykelkovar@mykelkovar.local>
2019-06-01 08:57:37 -0500

    wrapped app with context api

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,30 +1,8 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@aol.com",
-        phone: "555-555-5511"
-      },
-      {
-        id: 2,
-        name: "Pam Beasley",
-        email: "pam@theoffice.com",
-        phone: "111-222-3333"
-      },
-      {
-        id: 3,
-        name: "Nala Kovar",
-        email: "nalathepup@gmail.com",
-        phone: "010-555-1234"
-      }
-    ]
-  };
-
   deleteContact = id => {
     const { contacts } = this.state;
 
@@ -36,18 +14,23 @@ class Contacts extends Component {
   };
 
   render() {
-    const { contacts } = this.state;
-
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+                />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
