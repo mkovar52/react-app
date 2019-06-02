7b3c138f2f67bc21333d39314b4e82416943a6fd
Mykel Kovar <mykelkovar@mykelkovar.attlocal.net>
2019-06-02 10:07:38 -0500

    About/AddContact pages added, included react router

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="display-4">ABOUT PAGE</h1>
+      <p className="lead">Simple contact manager app</p>
+    </div>
+  );
+}
